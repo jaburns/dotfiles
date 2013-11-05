0ab4b101a4178bbf5eb21edb3d41b210c59fe111
Jeremy Burns <jeremyaburns@gmail.com>
2013-11-05 16:00:05 -0700

    Added _vimrc installation, and minttyrc link

--- link.sh
+++ link.sh
@@ -3,6 +3,11 @@
 # Get the script's running folder.
 repo_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+files_windows=$( cat <<EOF
+.minttyrc
+EOF
+)
+
 files_basic=$( cat <<EOF
 .bashrc
 .tmux.conf
@@ -24,10 +29,22 @@ do_files () {
         [ -a "$HOME/$x" ] && rm "$HOME/$x"
         mkdir -p "$(dirname "$HOME/$x")"
         ln "$repo_dir/$x" "$HOME/$x"
-        echo "$HOME/$x" "==>" "$repo_dir/$x"
+        echo "$repo_dir/$x" '==>' "$HOME/$x"
     done
 }
 
+echo ""
 echo "Linking $1 config files..."
 eval "do_files \$files_$1"
 echo "Done!"
+echo ""
+
+if [ "$1" == 'windows' ]; then
+    echo "Installing windows _vimrc file..."
+    rm -f "/cygdrive/c/Users/$(whoami)/_vimrc"
+    cp "$repo_dir/.vimrc" "/cygdrive/c/Users/$(whoami)/_vimrc"
+    echo "$repo_dir/.vimrc" '==>' "/cygdrive/c/Users/$(whoami)/_vimrc"
+    echo "Done!"
+    echo ""
+fi;
+
